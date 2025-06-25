48dc631c1a5ed98d6363e9925e571f7d211d7f61
dlrjsgml44 <leegeh1213@gmail.com>
2025-06-25 10:31:32 +0900

    GA4

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,7 +42,7 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GA_ID}');
+              gtag('config', ${GA_ID});
             `,
           }}
         />
